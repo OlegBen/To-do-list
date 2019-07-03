e8b60e9e46dd34b1087981e3a67e50763756a1c7
Oleg <ben.oleg.i.1.17@gmail.com>
2019-07-03 21:46:51 +0300

    Added basic functionality

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,7 +1,44 @@
 <template>
-    <h1>To do list</h1>
+    <div class="toDoList">
+        <input type="text" v-model.lazy="text">
+        <input type="time" v-model.lazy="time">
+        <button @click="addElement">Добавить</button>
+
+        <ul>
+            <li v-for="(elem, index) in arr">
+                <div class="wrapper">
+                    {{ index }}: {{ elem.text }} - {{ elem.time }}
+                    <button @click="deleteElement(index)">Удалить</button>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-
+    export default {
+        data() {
+            return {
+                text: '',
+                time: '',
+                arr: []
+            }
+        },
+        methods: {
+            addElement: function() {
+                if(this.text !== '' && this.time !== '') {
+                    let resObj = {
+                        text: this.text,
+                        time: this.time
+                    };
+                    this.arr.push(Object.assign({}, resObj));
+                }
+                this.text = '';
+                this.time = '';
+            },
+            deleteElement: function(index) {
+                this.arr.splice(index,1);
+            }
+        }
+    }
 </script>
